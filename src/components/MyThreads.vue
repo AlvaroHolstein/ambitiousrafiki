599e4d31d4c4b55a5b80277c21d85c3a5acb4d4f
unknown <j_henrique_campos@hotmail.com>
2019-06-05 18:59:37 +0100

    remover imagem do preview

--- src/components/MyThreads.vue
+++ src/components/MyThreads.vue
@@ -3,32 +3,33 @@
     <div v-for="(thread, cont) in threads" v-bind:key="cont">
       <h4>
         <strong>
-          <a>{{thread.title}}</a>
+          <a>{{ thread.title }}</a>
         </strong>
       </h4>
 
       <div class="span6">
         <!--Descrição-->
-        <p v-html="thread.question"></p>
+        <p v-html="removeImageTag(thread.question)"></p>
         <p>
           <router-link
-            :to="{name:'thread',params:{threadid:thread.id}}"
-            :class="{'btn btn-primary':true}"
-          >Read More</router-link>
+            :to="{ name: 'thread', params: { threadid: thread.id } }"
+            :class="{ 'btn btn-primary': true }"
+            >Read More</router-link
+          >
           <!--Depois alterar-->
         </p>
       </div>
 
       <p>
         <i class="icon-calendar"></i>
-        {{thread.date}}
+        {{ thread.date }}
         <!--Data-->
         |
         <i class="icon-tags"></i> Tags :
         <!--TAGS-->
         <a v-for="(tag, cont) in thread.tags" v-bind:key="cont">
           <!---->
-          <span class="label label-info">{{tag.text}}</span>
+          <span class="label label-info">{{ tag.text }}</span>
         </a>
       </p>
     </div>
